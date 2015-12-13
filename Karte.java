678de1bf71fa6acfb3f8322bcaf23f742627c083
HTL-20120161 <lukas.gritsch@students.htlinn.ac.at>
2015-12-13 17:41:26 +0100

    Update Karte.java

--- Karte.java
+++ Karte.java
@@ -1,27 +1,47 @@
-:
-
-:help
+import java.util.Random;
 
 
-:
+public class Karte {
+	public String farbe;
+	public String nr;
+	
+	Karte(){}
+
+
+	Karte(String farbe, String nr)
+	{
+		this.farbe = farbe;
+		this.nr = nr;
+	}
+	
+	//Gets und sets
+
+	public String getFarbe() {
+		return farbe;
+	}
+
+
+	public void setFarbe(String farbe) {
+		this.farbe = farbe;
+	}
+
+
+	public String getNr() {
+		return nr;
+	}
+
+
+	public void setNr(String nr) {
+		this.nr = nr;
+	}
+
+
+	
+		
+	
+
+}
 
 
 
-
-
-
-	:
-	hallo
-
-
-
-
-
-
-
-	:q!
-des iatz ah no
-
-
-This is a Test
 
